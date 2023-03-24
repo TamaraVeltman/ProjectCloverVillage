5f383124cfecc8748b6aa915d230bc7e45570add
HiroMooshroom <124800222+HiroMooshroom@users.noreply.github.com>
2023-03-24 13:25:03 +0100

    ConvManeger
    Kleine aanpassing

--- ProjectCloverVillage/Assets/Conversations scripts/ConvManeger.cs
+++ ProjectCloverVillage/Assets/Conversations scripts/ConvManeger.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ConvManeger : MonoBehaviour
 {
     public Conversation currentConversation;
+    public TMP_Text textOnScreen;
 
     // Start is called before the first frame update
     void Start()
     {
-        print(currentConversation.conversation[1]);
+        print(currentConversation.conversation[0]);
+
+        textOnScreen.text = currentConversation.conversation[0];
     }
 
     // Update is called once per frame
