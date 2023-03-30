24225a726aa0e1dca9dcbf84d3e867d10e88824f
HiroMooshroom <124800222+HiroMooshroom@users.noreply.github.com>
2023-03-30 16:03:07 +0200

    Few conv changes

--- ProjectCloverVillage/Assets/Conversations scripts/ConvManeger.cs
+++ ProjectCloverVillage/Assets/Conversations scripts/ConvManeger.cs
@@ -11,9 +11,9 @@ public class ConvManeger : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print(currentConversation.conversation[0]);
+        
 
-        textOnScreen.text = currentConversation.conversation[0];
+        textOnScreen.text = currentConversation.conversation[1];
     }
 
     // Update is called once per frame
