0b8e7fbed0b227364042e736a18540ef75179458
TamaraVeltman <124800131+TamaraVeltman@users.noreply.github.com>
2023-04-03 13:57:23 +0200

    Scripts
    Videoscript

--- ProjectCloverVillage/Assets/UI/Video.cs
+++ ProjectCloverVillage/Assets/UI/Video.cs
@@ -1,17 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Video : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider qualitySlider;
+    public float brightness = 1.0f;
+    private Material material;
+
+    public void ToggleFullscreen()
     {
-        
+        Screen.fullScreen = !Screen.fullScreen;
+        //met deze code regel toggle je fullscreen of windowedscreen.
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetQuality()
+    {
+        QualitySettings.SetQualityLevel((int)qualitySlider.value);
+        Debug.Log((int)qualitySlider.value);
+        //Veranderd de quality van de game met een slider 0 = Performant, 1 = Balanced, 2 = High fidelity.
+    }
+
+    public void Start()
+    { 
+
+    }
+
+    public void Update()
     {
         
     }
