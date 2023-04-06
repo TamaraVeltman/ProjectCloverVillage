9fb18c9ba31c3e5981f6ca3557bec107fd73d6b8
nathalielensen <124800083+nathalielensen@users.noreply.github.com>
2023-04-06 10:16:10 +0200

    Script updates
    Comments verbeterd (dit is niet belangrijk ofzo voor het spel zelf, dus negeer het maar)

--- ProjectCloverVillage/Assets/Nathalie/Scripts/PickupTest.cs
+++ ProjectCloverVillage/Assets/Nathalie/Scripts/PickupTest.cs
@@ -16,18 +16,17 @@ public class PickupTest : MonoBehaviour
         //opgeslagen in hit (public RaycastHit hit;), de afstand dat de Raycast is 1000.
         if (Physics.Raycast(transform.position, transform.forward, out hit, 1000))
         {
-            //Raycast raatk object met de naam Object 2.
+            //Als je met de raycast een object aan raakt genaamd "Object 2" kan je met E het object oppaken (Volgende regels code).
             if (hit.transform.name == "Object 2")
             {
-                //Je klikt op het object dat het goede is (dus die met de naam Object 2, in dit geval is dat de sphere) wanneer je dit doet gebeurt de code eronder.
-                //Met GetMouseButtonDown zorg je ervoor dat de raycast zelf niet het object al uit zichzelf verwijderd en dat je dit zelf moet doen.
-                //De 0 achter GetMouseButtonDown betekent dat je met de linker muisknop moet drukken om er voor te zorgen dat de code werkt die erna komt.
-                if (Input.GetMouseButtonDown(0)) // 0 betekent de linker muisknop
+               //Als je het goeie object hebt geraakt met de raycast zou je het object nu kunnen oppakken, dit kan je met E doen op je toetsenbord.
+                //Het correcte object is Object 2, je kan dus alleen deze "oppaken" met E, de andere kunnen niet gepakt worden.
+                if (Input.GetKeyDown(KeyCode.E))
                 {
-                    //Wanneer het object is gezien door de raycast word dit in de console geprint.
-                    print("Object opgepakt!");
-                    //Als het object word geraakt door de raycast word hij gedetroyed, je hebt hem dan dus een soortvan "opgepakt".
+                    //Zodra je het object hebt "opgepakt" word het gameObject gedestroyed.
                     Destroy(hit.transform.gameObject);
+                    //Wanneer je het object zowaar hebt opgepakt, word dit in de console geprint, je zou dus nu verder moeten gaan met de quest.
+                    print("Continue met de Quest.");
                 }
 
             }
